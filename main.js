8dc11f340adfb345c47ae971f56c7623ca0db111
Redstonerayy <68440617+Redstonerayy@users.noreply.github.com>
2020-11-22 20:29:41 +0100

    Added Button and Input ot HTML File

--- main.js
+++ main.js
@@ -9,20 +9,20 @@ function createWindow () {
     }
   })
 
-  win.loadFile('index.html')
+  win.loadFile('index.html');
   //win.webContents.openDevTools()
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
 })
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    createWindow();
   }
 })
