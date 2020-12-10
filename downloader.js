83f853eca8c538f1b78c01016174a894f6084904
Redstonerayy <68440617+Redstonerayy@users.noreply.github.com>
2020-12-10 22:20:04 +0100

    Cant find ffmpeg.

--- downloader.js
+++ downloader.js
@@ -1,8 +1,10 @@
 //requirements
 const fs = require("fs");
 const ytdl = require("ytdl-core");
-var ffmpeg = require('fluent-ffmpeg');
-var command = ffmpeg();
+//const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
+//ffmpeg.setFfmpegPath(ffmpegPath);
+//var ffmpeg = require('fluent-ffmpeg');
 //dumb
 const textinput = document.getElementById("urlinput");
 const videoformat = document.getElementById("videofselect");
@@ -74,10 +76,8 @@ textinput.addEventListener("input", async() => {
     //getinfo
     let info = await ytdl.getInfo(textinput.value);
     //filter
-    let formats = filter_vid_aud_both(info.formats);
-    let formatsall = formats;
-    let formats = formats.filter((value) => { return !(value.hasaudio && value.hasvideo)}
-    });
+    let formatsall = filter_vid_aud_both(info.formats);
+    let formats = formatsall.filter((value) => { return !(value.hasaudio && value.hasvideo)});
     //filter for displaying
     //video
     let videoquali = formats[0].map(vheight => vheight.height);
@@ -99,12 +99,12 @@ textinput.addEventListener("input", async() => {
     for (let i = 0; i < aquali_unique.length; i++) {
       makeoption(audioformat, aquali_unique[i]);
     }
-    inputdata = [info, formatswithoutboth, videoquali, vquali_unique, audioquali, aquali_unique];
+    inputdata = [info, formats, videoquali, vquali_unique, audioquali, aquali_unique];
   } else {
     //remove all childs of the selects and add the None option back
     //because its not a YT link anymore
     videochildren = videoformat.children;
-    audiochilds = audioformat.children;
+    audiochildren = audioformat.children;
     for (let i = 0; i < videochildren.length; i++) {
       videochildren[i].remove();
     }
@@ -120,33 +120,33 @@ button[0].addEventListener("click", async() => {
   //download audio and video depending on the selections; defaults for None
   //writes 2 files
   if( (videoformat.value == "None") && (audioformat.value == "None")){
-    await downloadvidaud(path, inputdata[0].title, 136, "mp4", 18, "mp4");
+    var filepaths = await downloadvidaud(path, inputdata[0].videoDetails.title, 136, "mp4", 18, "mp4");
   } else if( ((videoformat.value == "None") && (audioformat.value != "None"))){
-    var aformat = inputdata[1].filter(value => value.audioBitrate == audioformat.value)[0];
-    filepaths = await downloadvidaud(path, inputdata[0].title, null, null, aformat.itag, aformat.container);
+    var aformat = inputdata[1][1].filter(value => value.audioBitrate == audioformat.value)[0];
+    filepaths = await downloadvidaud(path, inputdata[0].videoDetails.title, null, null, aformat.itag, aformat.container);
   } else if( ((videoformat.value != "None") && (audioformat.value == "None"))){
-    var vformat = inputdata[1].filter(value => value.height == videoformat.value)[0]
-    filepaths = await downloadvidaud(path, inputdata[0].title, vformat.itag, vformat.container, null, null);
+    var vformat = inputdata[1][0].filter(value => value.height == videoformat.value)[0]
+    filepaths = await downloadvidaud(path, inputdata[0].videoDetails.title, vformat.itag, vformat.container, null, null);
   } else {
-    var vformat = inputdata[1].filter(value => value.height == videoformat.value)[0]
-    var aformat = inputdata[1].filter(value => value.audioBitrate == audioformat.value)[0];
-    filepaths = await downloadvidaud(path, inputdata[0].title, vformat.itag, vformat.container, aformat.itag, aformat.container);
+    var vformat = inputdata[1][0].filter(value => value.height == videoformat.value)[0];
+    var aformat = inputdata[1][1].filter(value => value.audioBitrate == audioformat.value)[0];
+    var filepaths = await downloadvidaud(path, inputdata[0].videoDetails.title, vformat.itag, vformat.container, aformat.itag, aformat.container);
   }
-
+  console.log(filepaths);
   //merge with ffmpeg
   try {
     //create and input vid/aud if downloaded
     var video = ffmpeg();
-    if(fs.stat(filepaths[0], (err, stats) => { return stats.isFile()){
-      video.input(filepath[0]);
-
+    if(fs.stat(filepaths[0], (err, stats) => { return stats.isFile(); })){
+      console.log(filepaths[0]);
+      video.input(filepaths[0]);
     }
-    if(fs.stat(filepaths[1], (err, stats) => { return stats.isFile()){
-      video.input(filepath[1]);
+    if(fs.stat(filepaths[1], (err, stats) => { return stats.isFile(); })){
+      video.input(filepaths[1]);
     }
-    video.output(fs.createWriteStream(inputdata[0] + ".mp4"))
-
+    video.output(fs.createWriteStream(inputdata[0].videoDetails.title + ".mp4"));
+    video.run();
   } catch (e) {
-
+    console.log(e);
   }
 });
