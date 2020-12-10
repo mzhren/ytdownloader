81dc442524ec001da7c3a0aa8fafa3f9b3c31ee8
Redstonerayy <68440617+Redstonerayy@users.noreply.github.com>
2020-12-10 18:34:43 +0100

    Added the download part and a bit for ffmpeg.

--- downloader.js
+++ downloader.js
@@ -44,18 +44,18 @@ function hasvideo(format){
 }
 
 //download
-function downloadvidaud(itagvideo, itagaudio, path, name, container){
+function downloadvidaud(path, name, itagvideo, containerv, itagaudio, containera){// "", title, 18, mp4, 18, mp4,
   if(ytdl.validateURL(textinput.value)){
     if(itagvideo != null){
       ytdl(textinput.value, { quality: itagvideo})
-      .pipe(fs.createWriteStream(path + name + containerv));
+      .pipe(fs.createWriteStream(path + name + "v" + "." + containerv));
     }
     if(itagaudio != null){
       ytdl(textinput.value, { quality: itagaudio})
-      .pipe(fs.createWriteStream(path + name + containera));
+      .pipe(fs.createWriteStream(path + name + "a" + "." + containera));
     }
+    return [path + name + "v" + "." + containerv, path + name + "a" + "." + containera];
   }
-  return true;
 }
 
 //GUI
@@ -70,18 +70,20 @@ function makeoption(parent, value){
 //event listener
 textinput.addEventListener("input", async() => {
   if(ytdl.validateURL(textinput.value)){
+    //getinfo
     let info = await ytdl.getInfo(textinput.value);
     //filter
-    formats = filter_vid_aud_both(info.formats);
-    //filter for displaying; video
-    videoquali = formats[0].map(vheight => vheight.height);
-    vquali_unique = videoquali.filter((value, index, self) => self.indexOf(value) === index);
+    let formats = filter_vid_aud_both(info.formats);
+    //filter for displaying
+    //video
+    let videoquali = formats[0].map(vheight => vheight.height);
+    let vquali_unique = videoquali.filter((value, index, self) => self.indexOf(value) === index);
     vquali_unique.sort((a, b) => {
       return b-a;
     });
     //audio
-    audioquali = formats[1].map(bitrate => bitrate.audioBitrate);
-    aquali_unique = audioquali.filter((value, index, self) => self.indexOf(value) === index);
+    let audioquali = formats[1].map(bitrate => bitrate.audioBitrate);
+    let aquali_unique = audioquali.filter((value, index, self) => self.indexOf(value) === index);
     aquali_unique.sort((a, b) => {
       return b-a;
     });
@@ -93,14 +95,48 @@ textinput.addEventListener("input", async() => {
     for (let i = 0; i < aquali_unique.length; i++) {
       makeoption(audioformat, aquali_unique[i]);
     }
+    inputdata = [info, formats, videoquali, vquali_unique, audioquali, aquali_unique];
   } else {
     //remove all childs of the selects and add the None option back
+    //because its not a YT link anymore
+    videochildren = videoformat.children;
+    audiochilds = audioformat.children;
+    for (let i = 0; i < videochildren.length; i++) {
+      videochildren[i].remove();
+    }
+    for (let i = 0; i < audiochildren.length; i++) {
+      audiochildren[i].remove();
+    }
+    makeoption(videoformat, "None");
+    makeoption(audioformat, "None");
   }
 });
 
 button[0].addEventListener("click", async() => {
-  await downloadvidaud();
-  if((itagvideo != null) && (itagaudio != null)){
-    //merge
+  //download audio and video depending on the selections; defaults for None
+  //writes 2 files
+  if( (videoformat.value == "None") && (audioformat.value == "None")){
+    await downloadvidaud(path, inputdata[0].title, , "mp4", "mp4");
+  } else if( ((videoformat.value == "None") && (audioformat.value != "None")){
+    var aformat = inputdata[1].filter(value => return value.audioBitrate == audioformat.value)[0];
+    filepaths = await downloadvidaud(path, inputdata[0].title, null, null, aformat.itag, aformat.container);
+  } else if( ((videoformat.value != "None") && (audioformat.value == "None")){
+    var vformat = inputdata[1].filter(value => return value.height == videoformat.value)[0]
+    filepaths = await downloadvidaud(path, inputdata[0].title, vformat.itag, vformat.container, null, null);
+  } else {
+    var vformat = inputdata[1].filter(value => return value.height == videoformat.value)[0]
+    var aformat = inputdata[1].filter(value => return value.audioBitrate == audioformat.value)[0];
+    filepaths = await downloadvidaud(path, inputdata[0].title, vformat.itag, vformat.container, aformat.itag, aformat.container);
   }
+
+  //merge with ffmpeg
+  /*
+  try {
+    if(fs.stat(filepaths[0], (err, stats) => { return stats.isFile()}){
+      var video = new ffmpeg(path + )
+    }
+
+  } catch (e) {
+  }
+  */
 });
